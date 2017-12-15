944658c9c46271c39e1cfdabe58cc7514047af20
Thomas de Grivel <thoxdg@gmail.com>
2017-12-15 16:36:09 +0100

    debug window

--- gldemo.c
+++ gldemo.c
@@ -1,13 +1,15 @@
 
+#include <stdio.h>
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
 
 GLFWwindow *g_wnd;
+char g_close = 0;
 
 void close (GLFWwindow *wnd)
 {
   (void) wnd;
-  glfwSetWindowShouldClose(g_wnd, GLFW_TRUE);
+  g_close = -1;
 }
 
 void size (GLFWwindow *wnd, int w, int h)
@@ -22,14 +24,17 @@ void draw ()
   glClear(GL_COLOR);
 }
 
-void create_window (char full)
+int create_window (char full)
 {
   GLFWmonitor *m = NULL;
   if (full)
     m = glfwGetPrimaryMonitor();
-  g_wnd = glfwCreateWindow(800, 600, "gldemo", m, NULL);
+  if (!(g_wnd = glfwCreateWindow(800, 600, "gldemo", m, NULL)))
+    return -1;
+  printf("g_wnd %p\n", g_wnd);
   glfwSetWindowCloseCallback(g_wnd, close);
   glfwSetFramebufferSizeCallback(g_wnd, size);
+  return 0;
 }
 
 int main (int argc, char *argv[])
@@ -37,8 +42,9 @@ int main (int argc, char *argv[])
   (void) argc;
   (void) argv;
   if (glfwInit() == GLFW_TRUE) {
-    create_window(0);
-    while (!glfwWindowShouldClose(g_wnd)) {
+    if (create_window(0))
+      return 1;
+    while (!g_close) {
       glfwPollEvents();
       draw();
     }
